73aea8a3321e8f03e9543e008354740ab0d859de
Lee Jong Ann <dlwhddksiq@naver.com>
2021-04-29 09:35:42 +0900

    1번 문제 해설 추가

---
+++ 대표값.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("in4.txt",'rt')
+
+n = int(input())
+a = list(map(int , input().split()))
+
+average = round(sum(a)/n)
+min = 21470000
+
+for index , x in enumerate(a):
+
+    tmp = abs(x-average)
+    if tmp<min:
+        min = tmp
+        score=x
+        res=index+1
+    elif tmp == min:
+        if x>score:
+            score=x
+            res=index+1
+
+print(average,res)
+
+
+
+
