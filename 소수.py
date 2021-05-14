7455f6b8579c9248b81717c80d6ffa2180caf13d
Lee Jong Ann <dlwhddksiq@naver.com>
2021-05-14 21:00:08 +0900

    숫자만 추출 공부 및 해설 작성중

--- 소수.py
+++ 소수.py
@@ -2,15 +2,17 @@ import sys
 sys.stdin = open("in6.txt", "rt")
 
 n = int(input())
-
-ch = [0] * (n+1)
 cnt = 0
 
+ch = [0] * (n+1)
 for i in range(2,n+1):
 
-    if ch[i] == 0:
-        cnt +=1
-        for j in range(i ,n+1,i):
-            ch[j]=1
 
-print(cnt)
+    for j in range(i,n+1,i):
+        if ch[j] == 0 :
+            cnt+=1
+            print(ch[j],end=" ")
+
+
+
+
