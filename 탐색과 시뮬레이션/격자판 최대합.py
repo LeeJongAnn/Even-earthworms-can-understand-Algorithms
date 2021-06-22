3b39924752d6b4fbcb567cc379845adae54157b3
Lee Jong Ann <dlwhddksiq@naver.com>
2021-06-22 15:27:44 +0900

    스택 가장 큰수

--- 탐색과 시뮬레이션/격자판 최대합.py
+++ 탐색과 시뮬레이션/격자판 최대합.py
@@ -4,28 +4,29 @@ sys.stdin = open("in6.txt","rt")
 
 n = int(input())
 
-a = [list(map(int , input().split())) for _ in range(n)]
+a = [list(map(int ,input().split())) for _ in range(n)]
 
-largest = -21470000
 sum1 = 0
 sum2 = 0
+
+
+largest = -21470000
 for i in range(n):
     for j in range(n):
+
         sum1 += a[i][j]
         sum2 += a[j][i]
-
-if sum1 >largest:
-    largest = sum1
-if sum2 > largest:
-    largest = sum2
+    if sum1>largest:
+        largest = sum1
+    if sum2>largest:
+        largest = sum2
 
 for i in range(n):
-
     sum1 += a[i][i]
-    sum2 += a[i][-n-i-1]
+    sum2 += a[i][n-i-1]
 
-    if sum1>largest:
-        largest=sum1
-    if sum2>largest:
-        largest=sum2
+    if sum1 > largest:
+        largest = sum1
+    if sum2 > largest:
+        largest = sum2
 print(largest)
