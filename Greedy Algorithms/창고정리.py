d3f74a679a332d3b2d31d0a51ca2323e6932bc04
Lee Jong Ann <dlwhddksiq@naver.com>
2021-07-10 21:50:41 +0900

    그리디 추가

---
+++ Greedy Algorithms/창고정리.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("in3.txt","rt")
+L = int(input())
+num = list(map(int,input().split()))
+time = int(input())
+num.sort()
+
+
+for _ in range(time):
+    num[0]+=1
+    num[-1]-=1
+    num.sort()
+
+print(num[L-1]-num[0])
