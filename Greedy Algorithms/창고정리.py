9e262fce1fd5d924e8edc989530ee1700a41933d
Lee Jong Ann <dlwhddksiq@naver.com>
2021-08-03 20:17:34 +0900

    타이타닉에서 살아남기

--- Greedy Algorithms/창고정리.py
+++ Greedy Algorithms/창고정리.py
@@ -1,5 +1,6 @@
 import sys
-sys.stdin = open("in3.txt","rt")
+sys.stdin = open("in3.txt", "rt")
+
 L = int(input())
 num = list(map(int,input().split()))
 time = int(input())
@@ -7,8 +8,9 @@ num.sort()
 
 
 for _ in range(time):
-    num[0]+=1
-    num[-1]-=1
+
+    num[0] += 1
+    num[-1] -= 1
     num.sort()
 
-print(num[L-1]-num[0])
+print(num[L-1] - num[0])
--- Greedy Algorithms/회의실 배정.py
+++ Greedy Algorithms/회의실 배정.py
@@ -1,17 +1,20 @@
 import sys
 sys.stdin = open("in1.txt","rt")
-n =int(input())
+
+n = int(input())
 meeting = []
 
 for i in range(n):
-    s,e = map(int , input().split())
+    s,e = map(int,input().split())
     meeting.append((s,e))
-meeting.sort(key=lambda x : (x[1],x[0]))
+meeting.sort(key = lambda x : (x[1],x[0]))
+print(meeting)
 
 et = 0
 cnt = 0
+
 for s,e in meeting:
     if s>=et:
-        et=e
-        cnt+=1
+        et = e
+        cnt += 1
 print(cnt)
