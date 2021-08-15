169868f561fd6904b6316f8edaae6d7ccbd2eb4d
Lee Jong Ann <dlwhddksiq@naver.com>
2021-08-15 20:11:28 +0900

    백준 잃어버린 괄호 추가

---
+++ Greedy Algorithms/잃어버린 괄호 - 백준 1541번.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("in8.txt",'rt')
+
+a = input().split('-')
+
+num = []
+
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1,len(num)):
+    n -= num[i]
+
+print(n)
+
