5929adec3a7c07f39891f282abb8cb84467b11e8
LeeJongAnn <dlwhddksiq@naver.com>
2021-10-06 19:23:32 +0900

    백준 캠핑 내용 추가

--- Greedy Algorithms/캠핑 - 백준 4796번.py
+++ Greedy Algorithms/캠핑 - 백준 4796번.py
@@ -1,9 +1,14 @@
 import sys
 sys.stdin = open("in15.txt", "rt")
 
+l,p,v = map(int ,input().split())
 
-tmp = []
-for i in range(3):
-    first = list(map(int ,input().split()))
-    tmp.append(first)
-for j in tmp:
+count = int(v//p)
+intro = int(v%p)
+result = (count * l) + intro
+
+count2 = int(v//p)
+intro2 = int(v%p)
+result2 = (count2 * l) + intro
+print("Case 1: " + str(result))
+print("Case 2: " + str(result2))
