3f2a5f7cae6212427fca3c7bca6746cc5b0adef4
LeeJongAnn <dlwhddksiq@naver.com>
2021-10-06 19:33:56 +0900

    백준 로프 추가

--- Greedy Algorithms/로프 - 백준 2217번.py
+++ Greedy Algorithms/로프 - 백준 2217번.py
@@ -2,12 +2,12 @@ import sys
 sys.stdin = open('in12.txt','rt')
 
 n = int(input())
-tmp = []
-results  = 0
-for i in range(n):
-    L = int(input())
-    tmp.append(L)
-    tmp.sort()
-    if tmp[i]<tmp[i+1]:
-        results = tmp[i]*2
-print(results)
+a = int(input())
+b = int(input())
+result = 0
+if a<b:
+    result = a*2
+else:
+    result = b*2
+
+print(result)
