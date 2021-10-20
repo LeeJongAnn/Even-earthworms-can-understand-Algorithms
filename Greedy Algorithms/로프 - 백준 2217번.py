6b055a436d49cc2a79832cd4749ce0494dfe9773
dlwhddksiq <dlwhddksiq@naver.com>
2021-10-20 15:08:54 +0900

    백준 2217번 로프 수정

--- Greedy Algorithms/로프 - 백준 2217번.py
+++ Greedy Algorithms/로프 - 백준 2217번.py
@@ -1,13 +1,25 @@
 import sys
 sys.stdin = open('in12.txt','rt')
-
-n = int(input())
-a = int(input())
-b = int(input())
-result = 0
-if a<b:
-    result = a*2
-else:
-    result = b*2
-
+N = int(input())
+rope = [int(sys.stdin.readline()) for i in range(N)]
+rope.sort()
+result=0
+for i in range(len(rope)):
+    if result < N * rope[i]:
+        result = N * rope[i]
+        N -= 1
+    else:
+        N -= 1
 print(result)
+
+# n = int(input())
+# a = int(input())
+# b = int(input())
+# result = 0
+#
+# for i in range(n):
+#     if a<b:
+#         result += a
+#     else:
+#         result += b
+# print(result)
