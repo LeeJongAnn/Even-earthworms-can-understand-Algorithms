1a27bae05f0fcf1f99ecf272b7c1fad89917ce41
dlwhddksiq <dlwhddksiq@naver.com>
2022-01-21 16:48:34 +0900

    곱하기 아니면 더하기 및 모험가 길드 수정

--- Greedy Algorithms/곱하기아니면더하기.py
+++ Greedy Algorithms/곱하기아니면더하기.py
@@ -1,20 +1,18 @@
 import sys
-sys.stdin = open("in20.txt","rt")
-
-num = input()
-num = list(num)
-
-num.sort()
+import time
+start = time.time()
+sys.stdin = open("in20.txt", "rt")
+data = input()
+print(data)
+first = int(data[0])
+print(first)
 result = 0
-first = int(num[0])
-second = int(num[1])
-cnt = 0
-for i in range(len(num)-1):
-    if i == 0:
-        result = first + second
-        cnt+=1
-    elif i >=1:
-        result += int(num[i]) * int(num[i+1])
-        cnt+=1
-print(result)
-print(cnt)
+for i in range(1,len(data)):
+    num = int(data[i])
+    if num<=1 or first<=1:
+        first += num
+    else:
+        first *= num
+print(first)
+
+print("time :", time.time() - start)
--- Greedy Algorithms/모험가길드.py
+++ Greedy Algorithms/모험가길드.py
@@ -1,33 +1,19 @@
 import sys
-sys.stdin = open("in19.txt","rt")
-
+import time
+start = time.time()  # 시작 시간 저장
+sys.stdin = open("in19.txt", "rt")
 n = int(input())
-data = list(map(int, input().split()))
+data = list(map(int,input().split()))
 data.sort()
-
-result = 0
 count = 0
-
+result = 0
 for i in data:
+    print("i 의 값은" + str(i))
     count += 1
+    print("count의 값은" + str(count))
     if count >= i:
         result += 1
         count = 0
+    print("result의 값은" + str(result))
 print(result)
-
-# n = int(input())
-# num = list(map(int,input().split()))
-# num.sort(reverse=True)
-# print(num)
-#
-# first = num[0]
-# cnt = 0
-#
-# for i in range(1,first):
-#     if len(num) == 0:
-#         break
-#     elif first>num[i]:
-#         num.pop()
-#         cnt+=1
-# print(cnt)
-#
+print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
