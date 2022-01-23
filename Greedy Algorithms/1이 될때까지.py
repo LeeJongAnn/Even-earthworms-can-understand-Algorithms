87e25e4333709f4c43dce5ca432dae30858857b0
dlwhddksiq <dlwhddksiq@naver.com>
2022-01-23 12:21:16 +0900

    큰 수의 법칙 및 1이 될때까지 - 다음에는 만들 수 없는 금액과 볼링공 고르기 예정

--- Greedy Algorithms/1이 될때까지.py
+++ Greedy Algorithms/1이 될때까지.py
@@ -1,17 +1,18 @@
 import sys
-sys.stdin = open("in18.txt","rt")
+import time
+start = time.time()
+sys.stdin = open("in18.txt", "rt")
 n,k = map(int,input().split())
-result = 0
-
-while n >= k:
-    while n%k != 0:
+print(n,k)
+cnt = 0
+while n>=k:
+    while n%k!=0:
         n -= 1
-        result +=1
-    n //= k
-    result += 1
-
+        cnt +=1
+    n//=k
+    cnt+=1
 while n>1:
     n -= 1
-    result += 1
-print(result)
-
+    cnt +=1
+print(cnt)
+print("시간: " , start - time.time())
--- Greedy Algorithms/큰 수의 법칙.py
+++ Greedy Algorithms/큰 수의 법칙.py
@@ -1,24 +1,31 @@
 import sys
+import time
+start = time.time()
 sys.stdin = open("in16.txt","rt")
-
-a,b,c = map(int,input().split())
+a,b,c= map(int,input().split())
+print(a,b,c)
 num = list(map(int,input().split()))
-
-num.sort(reverse=True)
-
-first = num[0]
-second = num[1]
-result = 0
 print(num)
-
+num.sort(reverse=True)
+MostLarge = num[0]
+SecondLarge = num[1]
+print(MostLarge)
+print(SecondLarge)
+result = 0
+count = 0
+count2 = 0
 while True:
     for i in range(c):
         if b == 0:
             break
-        result += first
-        b -=1
+        b -= 1
+        result += MostLarge
+        count +=1
     if b == 0:
         break
-    result += second
-    b -= 1
+    b -=1
+    result += SecondLarge
+    count2 += 1
 print(result)
+print("큰 수는:" ,count,"작은 수는:",count2)
+print("걸린 시간:" ,time.time() - start)
