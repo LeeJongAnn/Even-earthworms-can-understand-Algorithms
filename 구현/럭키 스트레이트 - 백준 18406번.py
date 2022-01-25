beb03ccea89bc488df6d20a2aa5bc151f933daa6
dlwhddksiq <dlwhddksiq@naver.com>
2022-01-25 13:43:52 +0900

    백준18406 럭키 스트레이트

--- 구현/럭키 스트레이트 - 백준 18406번.py
+++ 구현/럭키 스트레이트 - 백준 18406번.py
@@ -1,17 +1,21 @@
 import sys
+import time
+start = time.time()
 sys.stdin = open("11.txt",'rt')
-n = (input())
+n = input()
+print(len(n))
 tmp = []
 left = 0
 right = 0
 cnt = 0
 for i in range((len(n)//2)):
     left += int(n[i])
-
-for i in range((len(n)//2)):
-    left -= int(n[i])
-
-if left == 0:
+for i in range((len(n)//2),len(n)):
+    right += int(n[i])
+print(left)
+print(right)
+if left == right:
     print("LUCKY")
 else:
     print("READY")
+print("시간" , time.time()-start)
