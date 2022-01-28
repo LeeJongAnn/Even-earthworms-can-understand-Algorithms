216907024ffb0641d6553309a03c5a9bdefc8f75
dlwhddksiq <dlwhddksiq@naver.com>
2022-01-28 14:43:41 +0900

    백준 보물 코드 수정 및 주소 추가 기타 날짜 오류 수정

--- Greedy Algorithms/보물 - 백준 1026번.py
+++ Greedy Algorithms/보물 - 백준 1026번.py
@@ -1,19 +1,16 @@
 import sys
 sys.stdin = open("in24.txt","rt")
 n = int(input())
+print(n)
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
-print(n)
 print(a)
 print(b)
 a.sort()
 print(a)
 result = 0
-for x in b:
-    for i in range(n):
-        result += a[i]*x
-        print(a[i],x)
-        print(result)
-
-
-
+for i in range(n):
+     result += min(a)*max(b)
+     a.pop(0)
+     b.pop(b.index(max(b)))
+print(result)
