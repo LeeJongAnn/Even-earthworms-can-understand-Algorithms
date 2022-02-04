1e440b50eb696ebba91dbf8d8eb0d97e848f7373
dlwhddksiq <dlwhddksiq@naver.com>
2022-02-04 15:00:31 +0900

    문자열 압축 수정

--- 구현/문자열 압축.py
+++ 구현/문자열 압축.py
@@ -1,21 +1,21 @@
 import sys
 sys.stdin = open("in15.txt","rt")
 a = input()
-def solution(s):
-    answer = len(s)
-    for step in range(1,len(s)//2 + 1):
-        compressed = ""
-        prev = s[0:step]
-        count = 1
-        for j in range(step,len(s),step):
-            if prev == s[j:j + step]:
-                count += 1
-            else:
-                compressed += str(count) + prev if count >= 2 else prev
-                prev = s[j:j + step]
-                count =  1
-        compressed += str(count) + prev if count >= 2 else prev
-        answer = min(answer,len(compressed))
-    print(answer)
-solution(a)
-
+answer = 1e9
+for step in range(1,len(a)//2+1):
+    compressed = ""
+    prev = a[0:step]
+    count = 1
+    for j in range(step,len(a),step):
+        if prev == a[j:j + step]:
+            count +=1
+            print("이전의 수 : ",prev)
+            print("압축 가능 카운트: ", count)
+        else:
+            compressed += str(count) + prev if count >=2 else prev
+            prev = a[j:j + step]
+            count = 1
+            print("압축문자열 : " ,compressed)
+    compressed += str(count) + prev if count >= 2 else prev
+    answer = min(answer,len(compressed))
+print(answer)
