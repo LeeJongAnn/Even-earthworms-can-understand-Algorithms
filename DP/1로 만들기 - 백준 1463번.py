29c600ef9ce4bd770660819e643928c4afed3ba8
dlwhddksiq <dlwhddksiq@naver.com>
2022-02-10 20:40:56 +0900

    리드미 주소 추가

--- DP/1로 만들기 - 백준 1463번.py
+++ DP/1로 만들기 - 백준 1463번.py
@@ -1,16 +1,12 @@
 import sys
 sys.stdin = open("first.txt", 'rt')
 n = int(input())
-
 result = 0
 cnt = 0
 while n>=1:
-    if n%3 != 0:
+    if n%3!=0:
         n -= 1
-        result = n//3
-        cnt += 1
-    if result%2 != 0:
-        n -= 1
-        cnt += 1
-        result //=2
+        cnt+= 1
+    n//=3
+    cnt +=1
 print(cnt)
