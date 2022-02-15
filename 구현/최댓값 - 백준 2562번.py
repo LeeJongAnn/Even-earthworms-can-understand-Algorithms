d73557a6c15b0b9ab9afe14c805bacfa1e9e37d3
dlwhddksiq <dlwhddksiq@naver.com>
2022-02-15 12:06:42 +0900

    백준 2562번 최댓값 수정

--- 구현/최댓값 - 백준 2562번.py
+++ 구현/최댓값 - 백준 2562번.py
@@ -2,12 +2,12 @@ import sys
 sys.stdin = open("in19.txt", 'rt')
 
 tmp = []
-for i in range(1,9):
+for i in range(1,10):
     n = int(input())
     tmp.append(n)
 print(tmp)
 
-index_result = tmp.index(max(tmp))+1
+index_result = tmp.index(max(tmp)) + 1
 result = max(tmp)
 print(result)
 print(index_result)
