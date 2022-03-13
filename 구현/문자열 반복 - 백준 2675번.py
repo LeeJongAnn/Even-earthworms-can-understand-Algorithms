8262067584cfcf26728c6adc5db2cef76e0a433c
dlwhddksiq <dlwhddksiq@naver.com>
2022-03-13 18:17:55 +0900

    문자열 반복 수정 및 리드미 수정

--- 구현/문자열 반복 - 백준 2675번.py
+++ 구현/문자열 반복 - 백준 2675번.py
@@ -4,14 +4,22 @@ import time
 start = time.time()
 sys.stdin = open("in23.txt", 'rt')
 n = int(input())
-tmp,tmp2 = [],[]
-a,b = input().split()
-for j in range(int(a)):
-    tmp.append(b[j]*int(a))
-result1 = ''.join(tmp)
-print(result1)
-a,b = input().split()
-for k in range(int(a)-1):
-    tmp2.append(b[k]*int(a))
-result2 = ''.join(tmp2)
-print(result2)
+for i in range(n):
+    x, y = input().split()
+    text = ''
+    for i in y:
+        text += int(x) * i
+    print(text)
+
+# n = int(input())
+# tmp,tmp2 = [],[]
+# a,b = input().split()
+# for j in range(int(a)):
+#     tmp.append(b[j]*int(a))
+# result1 = ''.join(tmp)
+# print(result1)
+# a,b = input().split()
+# for k in range(int(a)-1):
+#     tmp2.append(b[k]*int(a))
+# result2 = ''.join(tmp2)
+# print(result2)
