897c00e1b3b38be4d413cf37cfccda42e0973b89
dlwhddksiq <dlwhddksiq@naver.com>
2022-03-18 15:54:27 +0900

    신입사원 선발 수정

--- Greedy Algorithms/신입사원 선발 - 백준 1946번.py
+++ Greedy Algorithms/신입사원 선발 - 백준 1946번.py
@@ -1,25 +1,40 @@
 import sys
 sys.stdin = open("in29.txt", "rt")
-n = input()
-m = int(input())
-tmp = []
-temp2 = []
-for i in range(m):
-    data = list(map(int, input().split()))
-    tmp.append(data)
-tmp.sort(reverse=True)
-print("첫번째 :",tmp)
-
-k = int(input())
-for j in range(k):
-    data = list(map(int, input().split()))
-    temp2.append(data)
-temp2.sort(reverse=True)
-print("두번째 :", temp2)
-
-largest = 0
-cnt = 0
-for x, y in tmp:
-
-print(cnt)
-
+n = int(input())
+first = []
+second = []
+for _ in range(n):
+    m = int(input())
+    for i in range(m):
+        a,b = map(int,input().split())
+        first.append((a,b))
+    print(first)
+    first.sort(reverse=True)
+    print(first)
+    cnt = 0
+    for x,y in first:
+        largest = 0
+        if largest<y:
+            largest = y
+            cnt += 1
+    print(cnt)
+# tmp = []
+# temp2 = []
+# for i in range(m):
+#     data = list(map(int, input().split()))
+#     tmp.append(data)
+# tmp.sort(reverse=True)
+# print("첫번째 :",tmp)
+#
+# k = int(input())
+# for j in range(k):
+#     data = list(map(int, input().split()))
+#     temp2.append(data)
+# temp2.sort(reverse=True)
+# print("두번째 :", temp2)
+#
+# largest = 0
+# cnt = 0
+#
+# print(cnt)
+#
