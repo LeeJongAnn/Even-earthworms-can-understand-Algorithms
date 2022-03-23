c5c43ca333feaa523ff5dca68b94db87e9884744
dlwhddksiq <dlwhddksiq@naver.com>
2022-03-23 17:11:15 +0900

    안되네;

--- DP/1로 만들기 - 백준 1463번.py
+++ DP/1로 만들기 - 백준 1463번.py
@@ -1,12 +1,20 @@
 import sys
-sys.stdin = open("first.txt", 'rt')
+sys.stdin = open("in1.txt", 'rt')
 n = int(input())
-result = 0
-cnt = 0
-while n>=1:
-    if n%3!=0:
+result =0
+cnt =0
+while n>3:
+    while n%3!=0:
         n -= 1
-        cnt+= 1
-    n//=3
-    cnt +=1
+        cnt +=1
+        n//=3
+        cnt +=1
+    while n%2!=0:
+        n -=1
+        cnt +=1
+        n//=2
+        cnt +=1
+    if n%3!=0 and n%2!=0:
+        n -= 1
+        cnt +=1
 print(cnt)
