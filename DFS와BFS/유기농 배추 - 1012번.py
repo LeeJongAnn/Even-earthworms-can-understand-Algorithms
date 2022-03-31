08d398ff81b5d73f004cfede9c1ff28c04fea38c
dlwhddksiq <dlwhddksiq@naver.com>
2022-03-31 16:32:48 +0900

    유기농 배추 안되네

--- DFS와BFS/유기농 배추 - 1012번.py
+++ DFS와BFS/유기농 배추 - 1012번.py
@@ -4,13 +4,41 @@ sys.stdin = open("in14.txt","rt")
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-t = int(input())
-data = []
-a,b,c = map(int,input().split())
-data = [[0] * b for _ in range(a)]
-for i in range(c):
-    x,y = map(int,input().split())
-    data[x][y] = 1
+def bfs(x,y,data):
+    q = deque()
+    q.append((x,y))
+    data[x][y] = value
+    count[value] = 1
 
-for q in data:
-    print(q)
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0>=nx or 0>=ny or ny>n or nx>n:
+                continue
+            if data[nx][ny] == value or data[nx][ny] == 0:
+                continue
+            data[nx][ny] = value
+            count[value] += 1
+    return
+
+value = 2
+count = {}
+
+
+t = int(input())
+for _ in range(t):
+    a,b,c = map(int,input().split())
+    data = [[0] * b for _ in range(a)]
+
+    for _ in range(a):
+        x,y = map(int,input().split())
+        data[x][y] = 1
+
+    for i in range(b):
+        for j in range(a):
+            if data[i][j] == 1:
+                bfs(i,j,data)
+
+
