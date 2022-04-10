7d9a08ab801059cc137b95ca9c9d3a8c1dc4c530
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-10 20:02:59 +0900

    유기농 배추 수정 및 리드미 수정

--- DFS와BFS/유기농 배추 - 1012번.py
+++ DFS와BFS/유기농 배추 - 1012번.py
@@ -1,44 +1,42 @@
 import sys
 from collections import deque
 sys.stdin = open("in14.txt","rt")
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
 
-def bfs(x,y,data):
-    q = deque()
-    q.append((x,y))
-    data[x][y] = value
-    count[value] = 1
-
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0>=nx or 0>=ny or ny>n or nx>n:
-                continue
-            if data[nx][ny] == value or data[nx][ny] == 0:
-                continue
-            data[nx][ny] = value
-            count[value] += 1
-    return
-
-value = 2
-count = {}
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
 
 t = int(input())
-for _ in range(t):
-    a,b,c = map(int,input().split())
-    data = [[0] * b for _ in range(a)]
 
-    for _ in range(a):
-        x,y = map(int,input().split())
-        data[x][y] = 1
+def bfs(graph, a, b):
+    queue = deque()
+    queue.append((a,b))
+    graph[a][b] = 0
 
-    for i in range(b):
-        for j in range(a):
-            if data[i][j] == 1:
-                bfs(i,j,data)
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+    return
 
+for i in range(t):
+    cnt = 0
+    n, m, k = map(int,input().split())
+    graph = [[0]*m for _ in range(n)]
 
+    for j in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+
+    for a in range(n):
+        for b in range(m):
+            if graph[a][b] == 1:
+                bfs(graph, a, b)
+                cnt += 1
+    print(cnt)
