f07dd0f10f1130817a457f73d967772cb67d16cc
dlwhddksiq <dlwhddksiq@naver.com>
2022-04-19 17:17:48 +0900

    머리 뜨겁네 진짜 답은 맞는데 왜 자꾸 틀린다고 할까

--- DFS와BFS/토마토 - 백준 7576번.py
+++ DFS와BFS/토마토 - 백준 7576번.py
@@ -9,29 +9,34 @@ graph = []
 for _ in range(k):
     graph.append(list(map(int,input().split())))
 
-for z in graph:
-    print(z)
-
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-q = deque()
+
+
 def bfs(x,y):
-    global count
-    count = 1
+    q = deque()
+    q.append((x,y))
     while q:
         x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<k:
+            if 0>nx or 0>ny or nx>=k or ny>=n:
                 continue
             if graph[nx][ny] == 0:
-                graph[nx][ny] = graph[y][x] + 1
+                graph[nx][ny] = graph[x][y] + 1
                 q.append((nx,ny))
-                count += 1
     return
-for i in range(n):
-    for j in range(k):
+
+for i in range(k):
+    for j in range(n):
         if graph[i][j] == 1:
-            q.append((i,j))
-print(count)
+            bfs(i,j)
+res = 0
+for z in graph:
+    for w in z:
+        if w == 0:
+            print(-1)
+            exit(0)
+    res = max(res,max(z))
+print(res-1)
