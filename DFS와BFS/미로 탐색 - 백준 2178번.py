d7c992280f54d8a2dd6f9179f616923085c37c26
dlwhddksiq <dlwhddksiq@naver.com>
2022-05-21 10:47:25 +0900

    미로 탐색 잘못돼서 수정

--- DFS와BFS/미로 탐색 - 백준 2178번.py
+++ DFS와BFS/미로 탐색 - 백준 2178번.py
@@ -1,44 +1,35 @@
 import sys
 from collections import deque
 sys.stdin = open("in17.txt","rt")
-
 n,k = map(int,input().split())
 
 graph = []
-
-
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-
 
 for _ in range(n):
     graph.append(list(map(int,input().strip())))
 
-for q in graph:
-    print(q)
-
-count = 1
-def bfs(x,y,graph):
+def bfs(x,y):
     q = deque()
     q.append((x,y))
-    graph[x][y] = value
-
     while q:
         x,y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<k and graph[nx][ny] == 1:
-                graph[nx][ny] = value
+            if 0>nx or 0>ny or nx>=n or ny>=k:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx,ny))
     return
 
-value = 1
-for i in range(n):
-    for j in range(k):
-        if graph[i][j] == 1:
-            bfs(i,j,graph)
-            count += 1
+bfs(0,0)
+for z in graph:
+    print(z)
 
-print(count)
+print(graph[n-1][k-1])
+
 
 
