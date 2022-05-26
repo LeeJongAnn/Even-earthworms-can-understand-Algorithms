db4c072910ca9eac5fa0f7474268586364a0aab2
dlwhddksiq <dlwhddksiq@naver.com>
2022-05-26 23:09:53 +0900

    평균 구하기 추가

---
+++ 기강다지기/평균 구하기.py
@@ -0,0 +1,14 @@
+
+arr = [1,2,3,4]
+
+def solution(arr):
+    answer = 0
+    array1 = []
+    for i in range(len(arr)):
+        array1.append(arr[i])
+        answer += array1[i]
+    print(array1)
+    answer = answer//len(arr)
+    return answer
+
+print(solution(arr))
