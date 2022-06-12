86ec6c92ce722aacd1a9ffba6516d82a7aaad415
dlwhddksiq <dlwhddksiq@naver.com>
2022-06-13 08:38:32 +0900

    콜라츠 추측 수정

--- 구현/콜라츠 추측.py
+++ 구현/콜라츠 추측.py
@@ -1,9 +1,23 @@
 def solution(num):
-    answer = 0
-    while num>1:
-        while num%2==0:
-            num//=2
-            if num % 2 != 0:
-                num = (num*3) + 1
-    return num
-print(solution(6))
+    count = 0
+    while num:
+        if num == 1:
+            break
+        if count == 500:
+            break
+        if num % 2 == 0:
+            num = num // 2
+            count += 1
+        else:
+            num = (num * 3) + 1
+            count +=1
+    return count if count != 500 else -1
+print(solution(626331))
+# def solution(num):
+#     answer = 0
+#     while num>1:
+#         while num%2==0:
+#             num//=2
+#         if num % 2 != 0:
+#             num = (num*3) + 1
+#     return num
